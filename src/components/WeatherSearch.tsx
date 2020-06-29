dd4baf4d81833f46d6b2d022cfbcaec984f221b7
Johan Abde <abdejohan@gmail.com>
2020-06-29 22:59:32 +0200

    Fix flexbox issues Add select boxes for user to input 1 or 5 day forecast

--- src/components/WeatherSearch.tsx
+++ src/components/WeatherSearch.tsx
@@ -7,6 +7,10 @@ const WeatherSearch = (props: any) => {
     <div className="weather-search">
       <form onSubmit={props.api_call} className="weather-search__form">
         <input name="userInput" className="weather-search__input" type="text" autoComplete="off" placeholder="Enter City.." required/>
+        <select name="days">
+          <option value="1day">1 day weather</option>
+          <option value="5day">5 day weather</option>
+        </select>
         <button className="weather-search__button">&rarr;</button>
       </form>
     </div>
