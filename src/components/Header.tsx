5ecf48916177145c6af704e360860b5733cf2760
Johan Abde <abdejohan@gmail.com>
2020-09-14 23:11:09 +0200

    Redsign header:   - Add darken effect to background image   - Add header text   - Adjust header text position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,11 @@ const Header = () => {
   return (
 
     <header className="header">
-      <div className="header__background"></div>
-      <p className="header__text">
-      </p>
+      <div className="header__background darken"></div>
+      <article className="header__text">
+        <p>Welcome to:</p>
+        <h3>YOUR DAILY FORECAST</h3>
+      </article>
     </header>
 
 
